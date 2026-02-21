ad4e85dd7d21dc8f0d48d05d67b224363e34c595
Daniel Paricio <danipaezgz@gmail.com>
2026-02-21 17:20:14 +0100

    Small changes in comments and methods

--- methods.py
+++ methods.py
@@ -4,18 +4,36 @@ from utils import one_hot_encode
 
 
 def softmax(S):
+    '''
+    This method computes the softmax of a matrix S normalizing along the row axis.
+    Input: S (n_samples, n_classes)
+    Output: softmax(S) (n_samples, n_classes) 
+    '''
     S_exp = np.exp(S - np.max(S, axis=1, keepdims=True))  
     return S_exp / np.sum(S_exp, axis=1, keepdims=True)
 
 
 def logsumexp(S):
+    '''
+    This method computes the log of the sum of exponentials of the input array S using
+    log sum_c exp^{s_c} = m + log sum_c exp^{s_c - m} with m = max_c s_c
+    which is numerically stable as it avoids overflow when S contains large values.
+    Input: S (n_samples, n_classes)
+    Output: logsumexp(S) (n_samples, 1)
+    '''
     m = np.max(S, axis=1, keepdims=True)
     return m + np.log(np.sum(np.exp(S - m), axis=1, keepdims=True))
 
 
 def cross_entropy_loss(Y_one_hot, S):
-    log_probs = S - logsumexp(S)
-    return -np.mean(np.sum(Y_one_hot * log_probs, axis=1))
+    '''
+    This method computes the cross-entropy loss given the one-hot encoded labels and the scores S.
+    Input: Y_one_hot (n_samples, n_classes)
+           S (n_samples, n_classes)
+    Output: cross_entropy_loss (scalar)
+    '''
+    logsoftmax = S - logsumexp(S) 
+    return -np.mean(np.sum(Y_one_hot * logsoftmax, axis=1))
 
 
 class RidgeRegression:
@@ -37,7 +55,7 @@ class RidgeRegression:
         K = self.kernel(X)
 
         # Solve for alpha using the closed-form solution
-        A = K + (self.lmbda + epsilon) * np.eye(K.shape[0])
+        A = K + (self.lmbda * X.shape[0] + epsilon) * np.eye(K.shape[0])
         # Since A is p.d we decompose A using Cholesky into L @ L.T
         L = np.linalg.cholesky(A)
         # First solve L @ T = Y_one_hot for T, then L.T @ alpha = T
@@ -70,12 +88,13 @@ class LogisticRegression:
             K_alpha = K @ alpha
             S = K_alpha
 
-            cel = cross_entropy_loss(Y_one_hot, S)
-            reg = 0.5 * (self.lmbda / n_samples) * np.sum(alpha * K_alpha)
+            cel = cross_entropy_loss(Y_one_hot, S) * n_samples
+            reg = 0.5 * self.lmbda * np.sum(alpha * K_alpha)
             loss = cel + reg
 
             P = softmax(S)
-            grad = (1 / n_samples) * (K @ (P - Y_one_hot)) + (self.lmbda / n_samples) * K_alpha
+            grad = K @ (P - Y_one_hot) + self.lmbda * K_alpha
+            
             return loss, grad.flatten()
 
         # Save data
@@ -89,14 +108,15 @@ class LogisticRegression:
 
         # Compute the kernel matrix
         K = self.kernel(X)
+        K = (K + K.T) / 2.0 # to ensure symmetry
 
-        alpha_init = np.zeros((K.shape[0], num_classes)).flatten()
+        alpha_init = np.zeros((K.shape[0], num_classes), dtype=np.float64).flatten()
         res = minimize(
             fun=_loss_and_grad,
             x0=alpha_init,
             jac=True,
             method='L-BFGS-B',
-            options={'maxiter': self.max_iter, 'ftol': self.tol}
+            options={'maxiter': self.max_iter}
             )
 
         self.alpha = res.x.reshape(-1, num_classes)
